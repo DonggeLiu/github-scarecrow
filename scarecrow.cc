dc2210c7e122614e61f70b74a095a8ec80d4bcce
Alan32Liu <donggeliu@google.com>
2022-04-04 12:30:29 +1000

    "Fix" the bug

--- scarecrow.cc
+++ scarecrow.cc
@@ -4,7 +4,6 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   if (size > 0 && data[0] == 'H')
     if (size > 1 && data[1] == 'I')
-       if (size > 2 && data[2] == '!')
-       __builtin_trap();
+       if (size > 2 && data[2] == '!');
   return 0;
 }
