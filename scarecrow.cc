e688329f39088cd90bdd7651b7adaa9e5c671143
Alan32Liu <donggeliu@google.com>
2022-06-30 14:48:17 +1000

    A target vulnerable to shell injections (and nothing else)

--- scarecrow.cc
+++ scarecrow.cc
@@ -1,9 +1,22 @@
-#include <stdint.h>
-#include <stddef.h>
+#include <string>
+extern "C" {
+  #include <string.h>
+}
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  if (size > 0 && data[0] == 'H')
-    if (size > 1 && data[1] == 'I')
-       if (size > 2 && data[2] == '!');
+void constructEchoCommand(const char *data, size_t size, char* command) {
+  std::string temp = std::string("echo ") + '"' + std::string(data, size) + '"';
+  strcpy(command, temp.c_str());
+}
+
+
+extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
+  if (data == NULL || size <= 0) {
+    return 0;
+  }
+  // command = echo "data"\0
+  char* command = (char*) malloc(5 + 1 + size + 1 + 1);
+  constructEchoCommand(data, size, command);
+  system(command);
+  free(command);
   return 0;
 }
